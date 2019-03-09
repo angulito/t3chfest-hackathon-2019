709fa127612b34d2196eb3b1dfa06f8b8ce4a8a5
angulito <natalia@returnly.com>
2019-03-09 13:52:29 +0100

    change default port

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.use(express.static(__dirname + '/build'));
 
 
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3001
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
